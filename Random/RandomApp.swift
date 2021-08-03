111931556133b338456da4e02b57f30533e8a158
Daniel Steinberg <daniel@dimsumthinking.com>
2021-08-03 16:02:53 -0400

    Initial Commit

---
+++ Random/RandomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomApp.swift
+//  Random
+//
+//  Created by Daniel Steinberg on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
