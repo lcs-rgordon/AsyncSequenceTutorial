b3f27421b4051ea0f7bac7607c1cba39ff3d5e70
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 10:19:50 -0400

    Created an AsyncIterator

--- Random/Async/AsyncModelIterator.swift
+++ Random/Async/AsyncModelIterator.swift
@@ -1,5 +1,8 @@
 import Foundation
 
-class AsyncModelIterator {
-
+class AsyncModelIterator: AsyncIteratorProtocol {
+    func next() async -> Int? {
+        sleep(1) // Sleep for one second, then return something
+        return Int.random(in: 1...99)
+    }
 }
