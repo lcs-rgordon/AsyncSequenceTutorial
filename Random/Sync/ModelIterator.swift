afffe2e49a92377f4ff8b96444dd9fbbb93a31d8
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 09:26:12 -0400

    Added an iterator to get five random numbers

--- Random/Sync/ModelIterator.swift
+++ Random/Sync/ModelIterator.swift
@@ -1,3 +1,13 @@
-class ModelIterator {
-  private(set) var count = 5
+// An iterator that gives you five elements between 1 and 99
+class ModelIterator: IteratorProtocol {
+    private(set) var count = 5
+    
+    func next() -> Int? {
+        guard count > 0 else {
+            return nil
+        }
+        count -= 1
+        return Int.random(in: 1...99)
+    }
+    
 }
