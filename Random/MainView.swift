217760ad21fb04803855eb39342e7d039f496738
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 10:34:16 -0400

    Generate random numbers using AsyncStream

--- Random/MainView.swift
+++ Random/MainView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct MainView {
   @State private var value: String = ""
-  let numbers = AsyncModel()
+  @State private var isRunning = false
 }
 
 extension MainView: View {
@@ -24,7 +24,23 @@ extension MainView {
   }
     
   private func startSequence() async {
+      let numbers = AsyncStream(Int.self) { continuation in // Building a stream of Ints
+          self.isRunning = true
+          
+          // Runs on a separate thread, detached
+          Task.detached {
+              while self.isRunning {
+                  sleep(1)
+                  // Put the result outside the task, so the "for await" can "hear" it
+                  continuation.yield(Int.random(in: 1...99))
+              }
+          }
+          
+      }
+      
+      // Wait for the next number to pop out of the stream
       for await number in numbers {
+          // Update the value
           self.value = number.description
       }
   }
