73d6e22e6f8811e43fa13e55a961e30ae63eb5ff
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 09:12:34 -0400

    Example of using an Iterator

--- Random/MainView.swift
+++ Random/MainView.swift
@@ -18,7 +18,16 @@ extension MainView: View {
 
 extension MainView {
   private func nextNumber() {
-    print("You need to connect to the model")
+      // Every sequence has an iterator
+      // Every iterator has a single method called next()
+      var iterator = model.numbers.makeIterator()
+      while let number = iterator.next() {
+          print(number.description)
+      }
+      // This is equivalent to the lines above
+//      for number in model.numbers {
+//          print(number.description)
+//      }
   }
 }
 
