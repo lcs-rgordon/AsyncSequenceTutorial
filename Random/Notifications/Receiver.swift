0d98a8b7ba7faba1b9c0d1b915f4cbcdfe877bdb
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 09:42:31 -0400

    Receiver now gets the notification of a new random number.

--- Random/Notifications/Receiver.swift
+++ Random/Notifications/Receiver.swift
@@ -1,5 +1,23 @@
 import Foundation
 
+// Publishes any new numbers that it receives notifications for
 class Receiver: ObservableObject {
   @Published var number: Int = 0
+}
+
+extension Receiver {
+    func registerForNotification() {
+        NotificationCenter.default.addObserver(forName: randomNumberNotification,
+                                               object: nil,
+                                               queue: nil) { notification in
+            
+            // What do we want to do with the notification?
+            // Make sure we have a Notifier object
+            // If not, just ignore the notification
+            if let object = notification.object as? Notifier {
+                // Get the number provided by the notification
+                self.number = object.number
+            }
+        }
+    }
 }
