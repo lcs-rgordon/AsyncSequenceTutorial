58de0bb98ff9599fddf70ae7ee4bcea52759135a
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 09:31:17 -0400

    Now we get an infinite stream of numbers between 1 and 99

--- Random/Sync/ModelIterator.swift
+++ Random/Sync/ModelIterator.swift
@@ -1,12 +1,7 @@
 // An iterator that gives you five elements between 1 and 99
 class ModelIterator: IteratorProtocol {
-    private(set) var count = 5
     
     func next() -> Int? {
-        guard count > 0 else {
-            return nil
-        }
-        count -= 1
         return Int.random(in: 1...99)
     }
     
