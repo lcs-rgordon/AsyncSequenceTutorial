c1cd8b82245300f12e2ec1ff7ca4975913bb4694
Russell Gordon <rgordon@lcs.on.ca>
2021-08-04 10:28:06 -0400

    Now we receive a number each second, just after pressing "next".

--- Random/Async/AsyncModel.swift
+++ Random/Async/AsyncModel.swift
@@ -1,3 +1,7 @@
-class AsyncModel {
-
+class AsyncModel: AsyncSequence {
+    typealias Element = Int     // Must state that each element in the sequence is an integer
+    
+    func makeAsyncIterator() -> AsyncModelIterator {
+        return AsyncModelIterator()
+    }
 }
--- Random/MainView.swift
+++ Random/MainView.swift
@@ -2,17 +2,13 @@ import SwiftUI
 
 struct MainView {
   @State private var value: String = ""
-  // Allows notifications to be posted
-  private let notifier = Notifier()
-   
-  // Whenever the published property changes, this will get the update
-  @StateObject private var receiver = Receiver()
+  let numbers = AsyncModel()
 }
 
 extension MainView: View {
   var body: some View {
     VStack (spacing: 40) {
-      Text(receiver.number.description)
+      Text(value)
       Button("Next",
              action: nextNumber)
     }
@@ -21,7 +17,16 @@ extension MainView: View {
 
 extension MainView {
   private func nextNumber() {
-      notifier.postRandomNumber()
+      // Put this in a task because pressing the "Next" button cannot be asycnronous
+      Task {
+          await startSequence()
+      }
+  }
+    
+  private func startSequence() async {
+      for await number in numbers {
+          self.value = number.description
+      }
   }
 }
 
